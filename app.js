0b1a84bed318ebebf3fadd513e28887ed78b305b
Bastien Pruvost <bastien.mory@gmail.com>
2022-05-08 16:06:38 +0200

    Add liquidjs and initialize app.js

---
+++ app.js
@@ -0,0 +1,26 @@
+const port = process.env.PORT || 3000;
+const morgan = require('morgan');
+const path = require('path');
+const express = require('express');
+const app = express();
+
+const index = require('./routes');
+
+// Render Liquid
+const { Liquid } = require('liquidjs');
+const engine = new Liquid();
+app.engine('liquid', engine.express());
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'liquid');
+// ---
+
+// Main middlewares
+app.use(morgan('short'));
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+// ---
+
+app.use(index);
+
+app.listen(port);
