123c001df6704a6e3c67bfabf621695945b253a0
Bastien Pruvost <bastien.mory@gmail.com>
2022-05-19 15:02:06 +0200

    update db password

--- database/index.js
+++ database/index.js
@@ -1,10 +1,13 @@
 const mongoose = require('mongoose');
 
 exports.clientP = mongoose
-  .connect('mongodb+srv://public:public-password@sweetter.zuhtu.mongodb.net/Sweetter?retryWrites=true&w=majority', {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-  })
+  .connect(
+    `mongodb+srv://public:${process.env.DB_PASS}@sweetter.zuhtu.mongodb.net/Sweetter?retryWrites=true&w=majority`,
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true
+    }
+  )
   .then(m => m.connection.getClient())
   .then(console.log('Connexion a MongoDB réussie !'))
   .catch(() => console.log('Connexion a MongoDB échouée...'));
